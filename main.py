259f248665314c486b9c9b0dad5bc65de3485c10
keremkoseoglu <kerem@keremkoseoglu.com>
2020-07-14 12:19:31 +0300

    needs a gui Fixes #2

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
-from yogaflow.reader.json_reader import JsonReader
-from yogaflow.generator.primal_generator import PrimalGenerator
-from yogaflow.yoga.yoga_class import YogaClass
-from yogaflow.yoga.yoga_style import YogaStyle
-from yogaflow.yoga.asana import AsanaDifficulty, AsanaStance
-from yogaflow.writer.html_writer import HtmlWriter
+"""Main entry point"""
+import os
+from PyQt5.Qt import QApplication
+import qdarkstyle
+from gui.prime import Prime
 
-yoga_class = YogaClass(
-    p_name="Test class",
-    p_style=YogaStyle.hatha,
-    p_difficulty=AsanaDifficulty.advanced,
-    p_duration=60,
-    p_asana_duration=2,
-    p_sequence=[AsanaStance.standing, AsanaStance.seated, AsanaStance.lying])
-
-reader = JsonReader()
-classes = reader.get_yoga_classes()
-pranayamas = reader.get_pranayamas()
-warmups = reader.get_warmups()
-asanas = reader.get_asanas()
-flows = reader.get_flows(asanas)
-meditations = reader.get_meditations()
-
-generator = PrimalGenerator()
-generator.generate(
-    p_yoga_class=yoga_class,
-    p_pranayamas=pranayamas,
-    p_warmups=warmups,
-    p_asanas=asanas,
-    p_flows=flows,
-    p_meditations=meditations)
-
-writer = HtmlWriter()
-writer.write(generated_class=yoga_class)
+APP = QApplication([])
+APP.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+P = Prime()
+os._exit(APP.exec_()) # pylint: disable=W0212
